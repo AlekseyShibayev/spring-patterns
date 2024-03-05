59a68829dff0106f6da2ec1cfd2a59ab2a39af91
Шибаев Алексей Евгеньевич (4094442) <VTB4094442@region.vtb.ru>
2024-03-05 23:55:09 +0300

    -remove TestEntityFactoryWithBeansBag

--- src/test/java/com/company/app/habr/test/HabrTest.java
+++ src/test/java/com/company/app/habr/test/HabrTest.java
@@ -2,13 +2,10 @@ package com.company.app.habr.test;
 
 import com.company.app.configuration.SpringBootTestApplication;
 import com.company.app.habr.domain.entity.Habr;
-import com.company.app.habr.domain.entity.HabrUser;
 import com.company.app.habr.domain.enums.Status;
 import com.company.app.habr.domain.repository.HabrRepository;
 import com.company.app.habr.domain.repository.HabrUserRepository;
 import com.company.app.habr.infrastructure.simple_creator.SimpleCreator;
-import com.company.app.habr.infrastructure.test_entity_factory_with_beans_bag.TestEntityFactoryWithBeansBag;
-import com.company.app.habr.infrastructure.test_entity_factory_with_beans_bag.service.TestEntityFactoryBeansBag;
 import com.company.app.habr.infrastructure.test_entity_factory_with_prototype.TestEntityFactoryWithPrototype;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
@@ -23,8 +20,6 @@ class HabrTest extends SpringBootTestApplication {
 
     @Autowired
     private SimpleCreator simpleCreator;
-    @Autowired
-    private TestEntityFactoryWithBeansBag testEntityFactoryWithBeansBag;
     @Autowired
     private TestEntityFactoryWithPrototype testEntityFactoryWithPrototype;
     @Autowired
@@ -46,40 +41,6 @@ class HabrTest extends SpringBootTestApplication {
             simpleCreator.addHabrUser(save, NAME);
             return save;
         });
-
-        Assertions.assertEquals(habr.getStatus().name(), Status.ON.name());
-        Assertions.assertEquals(1, habr.getHabrUsers().size());
-        Assertions.assertEquals(NAME, habr.getHabrUsers().get(0).getName());
-        Assertions.assertNotNull(habr.getHabrUsers().get(0).getId());
-    }
-
-    @Test
-    void step_2_testEntityFactory_test_with_lambda() {
-        Habr habr = testEntityFactoryWithBeansBag.habrBy(Status.ON)
-            .with(habrBuilderContext -> {
-                Habr minimumPosibleHabr = habrBuilderContext.getHabr();
-                TestEntityFactoryBeansBag beansBag = habrBuilderContext.getBeansBag();
-                HabrUserRepository habrUserRepository = beansBag.getHabrUserRepository();
-                HabrRepository habrRepository = beansBag.getHabrRepository();
-
-                HabrUser user = new HabrUser().setHabr(minimumPosibleHabr).setName(NAME);
-                habrUserRepository.save(user);
-                minimumPosibleHabr.getHabrUsers().add(user);
-                habrRepository.save(minimumPosibleHabr);
-            })
-            .createOne();
-
-        Assertions.assertEquals(habr.getStatus().name(), Status.ON.name());
-        Assertions.assertEquals(1, habr.getHabrUsers().size());
-        Assertions.assertEquals(NAME, habr.getHabrUsers().get(0).getName());
-        Assertions.assertNotNull(habr.getHabrUsers().get(0).getId());
-    }
-
-    @Test
-    void step_4_testEntityFactory_test_with_impl_as_method() {
-        Habr habr = testEntityFactoryWithBeansBag.habrBy(Status.ON)
-            .addHabrUser(NAME)
-            .createOne();
 
         Assertions.assertEquals(habr.getStatus().name(), Status.ON.name());
         Assertions.assertEquals(1, habr.getHabrUsers().size());
